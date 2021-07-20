6707bcc6fded24a215e471b276f0ffd871fb58b4
Nacho Duarte <nacho.duarte.97@gmail.com>
2021-07-20 02:13:23 -0300

    Agregados array de objetos y array de Items, optimizacion de código

--- js/main.js
+++ js/main.js
@@ -12,130 +12,87 @@ function Articulo(id, nombre, precio, cantidad){
     }
 }
 
-const articulo1 = new Articulo(1, "Caramelos", 100, 10);
-const articulo2 = new Articulo(2, "Miel 1/4", 115, 10);
-const articulo3 = new Articulo(3, "Miel 1/2", 225, 10);
-const articulo4 = new Articulo(4, "Miel kilo", 450, 10);
+const articulos = [];
+articulos.push(new Articulo(1, "Caramelos", 100, 10));
+articulos.push(new Articulo(2, "Miel 1/4", 115, 10));
+articulos.push(new Articulo(3, "Miel 1/2", 225, 10));
+articulos.push(new Articulo(4, "Miel kilo", 450, 10));
 
-let costoEnvio = 150;
+const compra = [];
+
+var costoEnvio = 150;
+var carrito = 1;
 
 const solicitarArticulo = function(){
-    return prompt("Ingrese el articulo deseado")
+    return prompt("Ingrese el articulo deseado.\nPuede solicitar un máximo de 4 artículos")
 }
 
 const suma = (a,b,c,d,e) => a + b + c + d + e;
 
-alert("LISTA DE PRECIOS" + "\n " +
-         articulo1.nombre + ": $" + articulo1.precio + " x 50 unidades" + "\n " + 
-         articulo2.nombre + ": $" + articulo2.precio + "\n " +
-         articulo3.nombre + ": $" + articulo3.precio + "\n " +
-         articulo4.nombre + ": $" + articulo4.precio);
 
-console.log(articulo1);
-console.log(articulo2);
-console.log(articulo3);
-console.log(articulo4);
+let lista = "LISTA DE PRECIOS";
+ for (let index = 0; index < articulos.length; index++) {
+     lista += "\n" + "N° " + articulos[index].id + " Nombre: " + articulos[index].nombre + " Precio: $" + articulos[index].precio; 
+ }
 
-let articuloIngresado1 = solicitarArticulo();
+alert(lista);
 
-if (articuloIngresado1 == "caramelos de propoleo"){
-    articulo1.vender(1);
-    alert("Compraste " + articulo1.nombre + " por $" + articulo1.precio + " x 50 unidades");
-}else if (articuloIngresado1 == "miel de cuarto kilo"){
-    articulo2.vender(1);
-    alert("Compraste " + articulo2.nombre + " por $" + articulo2.precio);
-}else if (articuloIngresado1 == "miel de medio kilo"){
-    articulo3.vender(1);
-    alert("Compraste " + articulo3.nombre + " por $" + articulo3.precio);
-}else if (articuloIngresado1 == "miel de kilo"){
-    articulo4.vender(1);
-    alert("Compraste " + articulo4.nombre + " por $" + articulo4.precio);
-}else{
-    alert("El producto solicitado no se encuentra en stock");
+let estaCompra = solicitarArticulo();
+
+while (carrito <= 4){
+    if (estaCompra == "caramelos de propoleo"){
+        compra.push(estaCompra);
+        articulos[0].vender(1);
+        carrito =(carrito + 1);
+        alert("Compraste " + articulos[0].nombre + " por $" + articulos[0].precio + " x 50 unidades");
+    }else if (estaCompra == "miel de cuarto kilo"){
+        compra.push(estaCompra);
+        articulos[1].vender(1);
+        carrito = (carrito + 1);
+        alert("Compraste " + articulos[1].nombre + " por $" + articulos[1].precio);
+    }else if (estaCompra == "miel de medio kilo"){
+        compra.push(estaCompra);
+        articulos[2].vender(1);
+        carrito = (carrito + 1);
+        alert("Compraste " + articulos[2].nombre + " por $" + articulos[2].precio);
+    }else if (estaCompra == "miel de kilo"){
+        compra.push(estaCompra);
+        articulos[3].vender(1);
+        carrito = (carrito + 1);
+        alert ("Compraste " + articulos[3].nombre + " por $" + articulos[3].precio);
+    }else{
+        alert("El producto solicitado no se encuentra en stock");
+    }
+    estaCompra = solicitarArticulo();
 }
 
+console.log(carrito);
+console.log(compra);
 
+console.log(articulos[0].cantidad);
+console.log(articulos[1].cantidad);
+console.log(articulos[2].cantidad);
+console.log(articulos[3].cantidad);
 
-let articuloIngresado2 = solicitarArticulo();
+articulos[0].reponer(1);
+articulos[1].reponer(1);
+articulos[2].reponer(1);
+articulos[3].reponer(1);
 
-if (articuloIngresado2 == "caramelos de propoleo"){
-    articulo1.vender(1);
-    alert("Compraste " + articulo1.nombre + " por $" + articulo1.precio + " x 50 unidades");
-}else if (articuloIngresado2 == "miel de cuarto kilo"){
-    articulo2.vender(1);
-    alert("Compraste " + articulo2.nombre + " por $" + articulo2.precio);
-}else if (articuloIngresado2 == "miel de medio kilo"){
-    articulo3.vender(1);
-    alert("Compraste " + articulo3.nombre + " por $" + articulo3.precio);
-}else if (articuloIngresado2 == "miel de kilo"){
-    articulo4.vender(1);
-    alert("Compraste " + articulo4.nombre + " por $" + articulo4.precio);
-}else{
-    alert("El producto solicitado no se encuentra en stock");
-}
+let sumaProductos = suma(articulos[0].precio, articulos[1].precio, articulos[2].precio, articulos[3].precio, costoEnvio);
 
-
-let articuloIngresado3 = solicitarArticulo();
-
-if (articuloIngresado3 == "caramelos de propoleo"){
-    articulo1.vender(1);
-    alert("Compraste " + articulo1.nombre + " por $" + articulo1.precio + " x 50 unidades");
-}else if (articuloIngresado3 == "miel de cuarto kilo"){
-    articulo2.vender(1);
-    alert("Compraste " + articulo2.nombre + " por $" + articulo2.precio);
-}else if (articuloIngresado3 == "miel de medio kilo"){
-    articulo3.vender(1);
-    alert("Compraste " + articulo3.nombre + " por $" + articulo3.precio);
-}else if (articuloIngresado3 == "miel de kilo"){
-    articulo4.vender(1);
-    alert("Compraste " + articulo4.nombre + " por $" + articulo4.precio);
-}else{
-    alert("El producto solicitado no se encuentra en stock");
-}
-
-let articuloIngresado4 = solicitarArticulo();
-
-if (articuloIngresado4 == "caramelos de propoleo"){
-    articulo1.vender(1);
-    alert("Compraste " + articulo1.nombre + " por $" + articulo1.precio + " x 50 unidades");
-}else if (articuloIngresado4 == "miel de cuarto kilo"){
-    articulo2.vender(1);
-    alert("Compraste " + articulo2.nombre + " por $" + articulo2.precio);
-}else if (articuloIngresado4 == "miel de medio kilo"){
-    articulo3.vender(1);
-    alert("Compraste " + articulo3.nombre + " por $" + articulo3.precio);
-}else if (articuloIngresado4 == "miel de kilo"){
-    articulo4.vender(1);
-    alert("Compraste " + articulo4.nombre + " por $" + articulo4.precio);
-}else{
-    alert("El producto solicitado no se encuentra en stock");
-}
-
-
-console.log(articulo1.cantidad);
-console.log(articulo2.cantidad);
-console.log(articulo3.cantidad);
-console.log(articulo4.cantidad);
-
-articulo1.reponer(1);
-articulo2.reponer(1);
-articulo3.reponer(1);
-articulo4.reponer(1);
-
-let sumaProductos = suma(articulo1.precio, articulo2.precio, articulo3.precio, articulo4.precio, costoEnvio);
-
-console.log(articulo1.cantidad);
-console.log(articulo2.cantidad);
-console.log(articulo3.cantidad);
-console.log(articulo4.cantidad);
+console.log(articulos[0].cantidad);
+console.log(articulos[1].cantidad);
+console.log(articulos[2].cantidad);
+console.log(articulos[3].cantidad);
 
 console.log(sumaProductos);
 
 
-alert("Caramelos de propoleo $ " + articulo1.precio + "\n " +
-      "Miel de cuarto kilo $ " + articulo2.precio + "\n " + 
-      "Miel de medio kilo $ " + articulo3.precio + "\n " +
-      "Miel de kilo $ " + articulo4.precio + "\n " +
+alert("Caramelos de propoleo $ " + articulos[0].precio + "\n " +
+      "Miel de cuarto kilo $ " + articulos[1].precio + "\n " + 
+      "Miel de medio kilo $ " + articulos[2].precio + "\n " +
+      "Miel de kilo $ " + articulos[3].precio + "\n " +
       "costo de envío $ " + costoEnvio + "\n " + 
       "TOTAL $ " + sumaProductos);
 
